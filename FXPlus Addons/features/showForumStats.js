fb9dcaaf5335ef3c6ff32e97736a2b242bdb4598
eilon40 <34318101+eilon40@users.noreply.github.com>
2024-07-04 09:57:35 +0300

    Delete FXPlus Addons directory

--- FXPlus Addons/features/showForumStats.js
+++
@@ -1,6 +0,0 @@
-//https://github.com/SilverTuxedo/FxPlusplus/blob/master/chrome/js/main_fxp.js#L347
-const total = document.querySelectorAll('#threads .threadtitle').length;
-elementReady("#forumStats i")
-	.then(element => {
-		element.innerHTML = "נתונים סטטיסטיים של " + total + " אשכולות:";
-	})
