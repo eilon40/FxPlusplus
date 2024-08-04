594217e920fefae91e88fa1709703bbf19854237
eilon40 <34318101+eilon40@users.noreply.github.com>
2024-08-04 09:35:46 +0300

    Add files via upload

--- FXPlus+ Addons/features/smileys.js
+++ FXPlus+ Addons/features/smileys.js
@@ -1,14 +1,26 @@
+//https://www.fxp.co.il/member.php?u=789624
+// Todo: share
+// Temporary
 const data = `
 https://yoursmiles.org/tsmile/heart/t4524.gif
 `
-if (document.querySelectorAll('.postbit').length > 0) {
+function waitForVBEditor() {
+	return new Promise(resolve => {
+		const interval = setInterval(() => {
+			if (typeof window.vB_Editor !== 'undefined') {
+				clearInterval(interval);
+				resolve(window.vB_Editor);
+			}
+		}, 100);
+	});
+}
+
+waitForVBEditor().then(object => {
 	const editor = Object.values(vB_Editor).at(0);
 	const images = data.trim().split('\n');
 	for (const image of images) {
 		if (!image) continue;
 		editor.config.smiley_descriptions.push(`[img]${image}[/img]`);
 		editor.config.smiley_images.push(`https://wsrv.nl/?url=${image}&w=30`);
-	}
-}
-// if (typeof CKEDITOR !== 'undefined') {}
-
+	}	
+});
